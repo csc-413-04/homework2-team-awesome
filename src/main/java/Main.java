2547ced5bc845349fe637f7332ef29853bbf7f9f
tahirdar <Tahirdar@mail.sfsu.edu>
2018-10-21 14:37:02 -0700

    Fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
       // staticFiles.externalLocation("public");
       // http://sparkjava.com/documentation
-      port(1234);
+      port(4321);
       // calling get will make your app start listening for the GET path with the /hello endpoint
       get("/hello", (req, res) -> "Hello World");
 
