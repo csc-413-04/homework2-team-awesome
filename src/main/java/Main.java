f2c1e79982469e226f5f03ee61e8728f3e17ec6f
tahirdar <Tahirdar@mail.sfsu.edu>
2018-10-21 15:12:48 -0700

    Updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,5 +10,9 @@ public class Main {
       // calling get will make your app start listening for the GET path with the /hello endpoint
       get("/hello", (req, res) -> "Hello World");
 
+      //newuser
+      get("/newuser", (req, res) -> "okay");
+      //user - Login
+      get("/user", (req, res) -> "Login Failed");
     }
 }
