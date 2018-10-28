7d22fb50905dc2e30a0b38550c903f011158663e
Shota Ebikawa <shotaebikawa@gmail.com>
2018-10-28 14:04:41 -0700

    added some mongodb codes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,27 @@
-package main.java;
+//package main.java
 import static spark.Spark.*;
-
+import com.mongodb.*;
 public class Main {
 
     public static void main(String[] args) {
-      // staticFiles.externalLocation("public");
-      // http://sparkjava.com/documentation
-      port(4321);
-      // calling get will make your app start listening for the GET path with the /hello endpoint
-      get("/hello", (req, res) -> "Hello World");
+        MongoClient client = new MongoClient("localhost",27017); //with default server and port adress
+        DB db = client.getDB( "REST2" );
+        DBCollection users = db.getCollection("usrs");
+        DBCollection auth = db.getCollection("auth");
+        // staticFiles.externalLocation("public");
+        // http://sparkjava.com/documentation
+        port(4321);
+        // calling get will make your app start listening for the GET path with the /hello endpoint
+        get("/hello", (req, res) -> "Hello World");
+        //newuser
+        get("/newuser/:user", (req, res) -> {
+           // System.out.println(req.)
+            return "hello " + req.params("user");
+        });
+        //user - Login
+        get("/user", (req, res) -> "Login Failed");
 
-      //newuser
-      get("/newuser", (req, res) -> "okay");
-      //user - Login
-      get("/user", (req, res) -> "Login Failed");
+
+
     }
 }
