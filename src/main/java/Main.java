fbe5735a73fd061997cafe966dffb63aa73f8336
Shota Ebikawa <shotaebikawa@gmail.com>
2018-11-01 17:01:11 -0700

    added ArrayList and Date libraries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.text.SimpleDateFormat;
 import static spark.Spark.*;
-
+import java.util.ArrayList;
+import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
